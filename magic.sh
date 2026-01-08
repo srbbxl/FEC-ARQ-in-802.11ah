5fd3592b9048d75fec83e3a95e7d9a3743b9a884
srbbxl <srbbxl@proton.me>
2026-01-08 18:54:57 +0100

    Delete magic.sh

--- magic.sh
+++
@@ -1,9 +0,0 @@
-find . -type f -name "*.py" ! -path "./.venv/*" -print0 \
-| sort -z \
-| while IFS= read -r -d '' file; do
-    echo "## ${file#./}"
-    echo '```python'
-    cat "$file"
-    echo '```'
-    echo
-done > all_output.md
